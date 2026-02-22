99877752e44ffd4e1902dc2b0f018006b87ba195
catsystemexe <238760990+catsystemexe@users.noreply.github.com>
2026-02-22 21:36:55 +0000

    default mesh bg stable

--- src/main.ts
+++ src/main.ts
@@ -298,15 +298,27 @@ async function main() {
           parallaxMul: 1,
           params: {
             mesh: {
-              amp: 0.22,
-              freq: 8.0,
-              speed: 0.25,
-              tilt: 0.9,
-              persp: 1.15,
-              xSpan: 2.8,
-              lineAlpha: 0.22,
-              gridX: 120,
-              gridZ: 80
+              amp: 0.24,  freq: 5.4,  speed: 0.23,
+              amp2: 0.12, freq2: 14.0, speed2: 0.55,
+
+              warpAmp: 0.24, warpFreq: 1.8, warpSpeed: 0.30,
+
+              bumpAmp: 0.11, bumpFreq: 16.0, bumpSpeed: 0.48, bumpSharp: 1.60,
+              
+              depthAmpPow: 1.0,
+
+              ampDepthNear: 0.120,
+              ampDepthFar:  12.6,
+              ampDepthBias: 0.72,
+              ampDepthPow:  2.6,
+
+              bumpDepthNear: 0.06,
+              bumpDepthFar:  2.25,
+              bumpDepthBias: 0.74,
+              bumpDepthPow:  2.6,
+
+              tilt: 1.65, persp: 1.48, xSpan: 2.8,
+              lineAlpha: 0.22, gridX: 120, gridZ: 80
             }
           }
         }
