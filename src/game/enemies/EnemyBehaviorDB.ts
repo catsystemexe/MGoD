cf1c76e933c00d57363f2ce5ca3f9e82eee79a18
catsystemexe <238760990+catsystemexe@users.noreply.github.com>
2026-06-28 13:47:31 +0200

    feat(enemies): expand movement preset library

--- src/game/enemies/EnemyBehaviorDB.ts
+++ src/game/enemies/EnemyBehaviorDB.ts
@@ -6,10 +6,12 @@ import { noneBehavior } from "./behaviors/none";
 import { straightBehavior } from "./behaviors/straight";
 import { sineBehavior } from "./behaviors/sine";
 import { invadersBehavior } from "./behaviors/invaders";
+import { zigzagBehavior } from "./behaviors/zigzag";
 
 export const EnemyBehaviorDB: Record<EnemyBehaviorId, EnemyBehavior> = {
   none: noneBehavior,
   straight: straightBehavior,
   sine: sineBehavior,
+  zigzag: zigzagBehavior,
   invaders: invadersBehavior,
 };
